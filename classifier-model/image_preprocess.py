16ce90c9595475130f8852bf5c9f9faf2ff9ffa7
Av-sek <abhishekghorashainee07@gmail.com>
2024-01-17 20:52:18 +0545

    dataset

--- classifier-model/image_preprocess.py
+++ classifier-model/image_preprocess.py
@@ -1,22 +1,13 @@
 import os
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+import os
 
 data_dir = "../garbage-large"
 
 classes = os.listdir(data_dir)
-
-"""
-    compare image size
-    Resizing the images to 256x256 and converting them to tensors
-
-    data augumatation(to be done)
-    grayscale v/s rgb ()
-
-    number of data in each class and augumentation on least number of data
-    dataset ko study garne
-    
-"""
 
 from torchvision import transforms
 
@@ -34,4 +25,34 @@ transformations = transforms.Compose([
 
 dataset = ImageFolder(data_dir, transform=transformations)
 
-print(dataset.classes)
+def count_images_in_subfolders(root_folder):
+    subfolders = [f.path for f in os.scandir(root_folder) if f.is_dir()]
+    counts = []
+
+    for subfolder in subfolders:
+        image_count = count_images_in_folder(subfolder)
+        counts.append((os.path.basename(subfolder), image_count))
+        print(f"Subfolder: {os.path.basename(subfolder)}, Number of Images: {image_count}")
+
+    total_images = sum(count for _, count in counts)
+    print(f"Total number of images: {total_images}")
+
+    # Plotting
+    plot_counts(counts)
+
+def count_images_in_folder(folder):
+    image_count = sum(1 for entry in os.scandir(folder) if entry.is_file())
+    return image_count
+
+def plot_counts(counts):
+    subfolders, image_counts = zip(*counts)
+    plt.bar(subfolders, image_counts)
+    plt.xlabel('Subfolders')
+    plt.ylabel('Number of Images')
+    plt.title('Image Count in Subfolders')
+    plt.xticks(rotation=45, ha='right')
+    plt.savefig("dataset.png")
+
+if __name__ == "__main__":
+    root_folder = "../garbage-large"  # Replace with the path to your folder
+    count_images_in_subfolders(root_folder)
